8b18106d78811cba5befad0812162c6e2ba38f4f
Behruz Tapdiqov <tapdigovbahruz@gmail.com>
2024-12-26 17:02:09 +0400

    first initialization

---
+++ src/test/java/com/example/bookconsoleappspring/BookConsoleAppSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookconsoleappspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookConsoleAppSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
